93f449e069aa16febe2b2b4903024f141311b076
Ronan Fitzpatrick <xronanfitzx@gmail.com>
2017-04-28 10:06:43 -0700

    first commit

--- Issues.js
+++ Issues.js
@@ -11,31 +11,47 @@ class Issues {
 
   // return an array containing each issues `id`
   get ids() {
+    return this.issues.map(issue => issue.id)
   }
 
   // return the total number of issues
   get numberOfIssues() {
+    let count = 1;
+    this.issues.reduce(issue => {
+      count++;
+    })
+    return count;
   }
 
   // return an array containing each issues `title`
   get titles() {
+    return this.issues.map(issue => issue.title);
   }
 
   // return an array containing the `login` property of the
   // `user` property for each issue. The array should not contain
   // any duplicate elements
   get users() {
+    let loginArr = new Set(this.issues.map(issue => issue.user.login));
+    return [...loginArr];
   }
 
   // return an array containing the `avatar_url` property of the
   // `user` property for each issue. The array should not contain
   // any duplicate elements
   get avatars() {
+    let avatars = new Set(this.issues.map(issue => issue.user.avatar_url));
+    return [...avatars];
   }
 
   // return an array containing all the `id` property of the `assignedIssue`
   // property for each issue with an `assignee` property that is not `null`
   get withAssignee() {
+    this.issues.map(issue => {
+      if (issue.assignee !== "null") {
+        return issue.id;
+      }
+    })
   }
 
   // return an array containing all the `id` property of the `assignedIssue`
